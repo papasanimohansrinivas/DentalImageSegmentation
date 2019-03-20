1de2fec7bf732466f0b1cddaf1a2fc3ca000f0d7
papasanimohansrinivas <papasani.mohansrinivas@gmail.com>
2019-03-20 14:57:28 +0530

    images

---
+++ download_dentalimages.sh
@@ -0,0 +1,8 @@
+
+#wget "https://mynotebook.labarchives.com/attachments/My45MDAwMDAwMDAwMDAwMDA0fDg1NzYyLzMvRW50cnlQYXJ0LzQxNTk4ODc0Nzh8OS44OTk5OTk5OTk5OTk5OTk=/1/original?sf324=343"
+
+#wget "https://data.mendeley.com/datasets/hxt48yk462/1/files/ba1c91b4-ee12-49f9-8783-7a36268478c2"
+
+#wget "https://mynotebook.labarchives.com/attachments/MTAuNHw4NTc2Mi84L0VudHJ5UGFydC8yODUyODA2Mzd8MjYuNA==/1/original?sf324=343"
+
+wget "https://data.mendeley.com/datasets/hxt48yk462/1/files/ba1c91b4-ee12-49f9-8783-7a36268478c2/DentalPanoramicXrays.zip?dl=1"
